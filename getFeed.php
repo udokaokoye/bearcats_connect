3e47ac0e365516042ccf9cedadfe9cdf0b8eb165
udoka <leviokoye.com>
2022-11-10 13:02:01 -0500

    rushed

--- getFeed.php
+++ getFeed.php
@@ -60,13 +60,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     'fName' => $row['firstName'],
                     'lName' => $row['lastName'],
                     'profile_picture' => $row['profile_picture'],
-                    'username' => $row['username']
-                    // 'comment' => $row['comments']
+                    'username' => $row['username'],
+                    // 'comment' => $row['comment']
                     // ! LATER WE WILL INCLUDE THE NUMBER OF COMMENTS ON THE POST AND THE LATEST COMMENT.
                 ]);
             }
+
+            foreach ($usersFeed as $key => $value) {
+                $postId = $value['id'];
+                $query = "SELECT * FROM comments LEFT JOIN users on comments.user_id=users.id WHERE `post_id`=$postId";
+                $result = mysqli_query($link, $query);
+                $comments = [];
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($comments, [
+
+                        'comment' => $row['comment'],
+                        'reply_id' => $row['reply_id'],
+                        'firstName' => $row['firstName'],
+                        'lastName' => $row['lastName'],
+                        'username' => $row['username'],
+                        'date' => $row['dateCreated'],
+                        
+                        ]);
+                }
+
+                if (count($comments)  > 0) {
+                    $index = array_search($postId, $usersFeed);
+                    $usersFeed[$index] = [
+                        $usersFeed[$index],
+                        'comment' => $comments
+                    ];
+                    echo json_encode( $index);
+                }
+
+            }
+
+            // ! SELECT ONLY THE IDs THEN DO A FOR EACH LOOP THROUGH ALL THE IDs AND RUN A QUERY LIKE THE ONE IN GET POST
+
+            // $query = "SELECT * FROM comments LEFT JOIN users on comments.user_id=users.id WHERE `post_id`=$postId";
+            // $result = mysqli_query($link, $query);
+            // $comments = [];
+            // while ($row = mysqli_fetch_assoc($result)) {
+            //     array_push($comments, [
+
+            //         'comment' => $row['comment'],
+            //         'reply_id' => $row['reply_id'],
+            //         'firstName' => $row['firstName'],
+            //         'lastName' => $row['lastName'],
+            //         'username' => $row['username'],
+            //         'date' => $row['dateCreated'],
+                    
+            //         ]);
+            // }
+
+            // $query = "SELECT 'firstName', 'lastName', 'username' FROM reactions LEFT JOIN users on reactions.user_id=users.id WHERE `post_id`=$postId";
+            // $result = mysqli_query($link, $query);
+            // $reactions = [];
+            // while ($row = mysqli_fetch_assoc($result)) {
+            //     array_push($reactions, [
+            //         'firstName' => $row['firstName'],
+            //         'lastName' => $row['lastName'],
+            //         'username' => $row['username'],
+            //         ]);
+            // }
         
-            echo json_encode($usersFeed);
+            // echo json_encode($usersFeed[8]);
         }
 } else {
     
